f1b5da324cc0548c1cb4a123c72a28c8e24c9aab
YousufR8 <111895919+YousufR8@users.noreply.github.com>
2022-12-06 12:36:45 -0500

    Add files via upload

---
+++ Bpa_test.py
@@ -0,0 +1,15 @@
+def Character ():
+    print ("Character")
+
+class Character:
+
+    class Hero:
+        levels = (1, 2, 3, 4, 5)
+
+
+player1 = ("knight, hp = 100, attack_damage = 50, lifespan = 3, Heroname = knight, loot = 50, IDnumber = 111, level = 1")
+print("knight, hp = 100, attack_damage = 50, lifespan = 3, Heroname = knight, loot = 50, IDnumber = 111, level = 1")
+
+player2 = ("Ogre, hp = 5500.0, attack_damage = 99.0, lifespan = 18.33, Heroname = Ogre, level = 2, IDnumber = 333")
+
+print ("Ogre, hp = 5500.0, attack_damage = 99.0, lifespan = 18.33, Heroname = Ogre, level = 2, IDnumber = 333")
